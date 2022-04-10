355f744a0dc2b11c983664cd0ed2205259d9188e
e195708 <e195708@ie.u-ryukyu.ac.jp>
2022-04-11 04:56:42 +0900

    test

--- discordbot.py
+++ discordbot.py
@@ -4,6 +4,11 @@ import traceback
 
 bot = commands.Bot(command_prefix='/')
 
+#メッセージ受信時に実行される処理
+@bot.event
+async def on_message(message):
+    #on_messageイベントの取得とコマンド機能を併用する際に必要な処理
+    await bot.process_commands(message)
 
 @bot.event
 async def on_command_error(ctx, error):
